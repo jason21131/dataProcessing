19a858dba3932f1635ca18177f77dc5177eae6ac
Ci Yang <jason21131@users.noreply.github.com>
2017-06-07 01:08:17 +0800

    Foutrh record
    Finished a lot of callback function to get map api info and create the csv file to store them

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyBAPeRnLPRB6FnWW9Iq6cq19x7NucuLqr4'
 });
 
-var stream = fs.createReadStream("houseAddress.csv", {encoding:'utf-8',bufferSize:11});
+var stream = fs.createReadStream("city-houseValue.csv", {encoding:'utf-8',bufferSize:11});	//houseAddress.csv
+var ws = fs.createWriteStream("my.csv", {defaultEncoding: 'utf8'});
 var houseData = [];
 var latlngString = '';
+var jsonArray = [];
 
 csv
  //.fromPath("houseAddress.csv")
@@ -21,10 +23,40 @@ csv
  })
  .on("end", function(){
      console.log("done");
-     useAPIs();
+     console.log(testArray);
+     getMoreInfomation(outputTheResult);
+     writingFile( writeCSV );
+     //useAPIs();
      //console.log("我是測試用中文");
      //console.log(houseData);
  });
+/*================================================*/
+
+var csvStream = csv.createWriteStream({headers: true}),
+    writableStream = fs.createWriteStream("my.csv", {defaultEncoding: 'utf8'});
+
+writableStream.on("finish", function(){
+  console.log("DONE!");
+});
+
+csv
+   .write([
+       ["a", "b"],
+       ["a1", "b1"],
+       ["a2", "b2"]
+   ], {headers: true})
+   .pipe(writableStream);
+
+// csvStream.pipe(writableStream);
+// csvStream.write({a: "a0", b: "b0"});
+// csvStream.write({a: "a1", b: "b1"});
+// csvStream.write({a: "a2", b: "b2"});
+// csvStream.write({a: "a3", b: "b4"});
+// csvStream.write({a: "a3", b: "b4"});
+// csvStream.end();
+
+
+/*================================================*/
 
 var geocodeInputObject = {
 	address:'',
@@ -35,10 +67,79 @@ var reverseGeocodeInputObject = {
 	language: 'zh-TW'
 }
 
+var testString = '嘉義縣太保市北港路二段429巷200弄31~60號';
+var testArray = [];
+testArray.push(testString);
+testArray.push('嘉義縣水上鄉南和村15鄰後寮61~90號');
+
+console.log(testArray);
+
+function getAddress( inputAdress, callbackFunction ){
+	geocodeInputObject.address = inputAdress;
+	var str;
+	googleMapsClient.geocode(geocodeInputObject, function(err, response){
+		str = response.json.results[0].geometry.location.lat;
+		str += ',';
+		str += response.json.results[0].geometry.location.lng;
+
+		callbackFunction( str );
+	});
+}
+
+function getMoreInfomation( callbackFunction ){
+	getAddress( houseData[6], function( output ){		//input the origin address #testString
+		console.log(output);
+		reverseGeocodeInputObject.latlng = output;
+		googleMapsClient.reverseGeocode( reverseGeocodeInputObject, function(err, response){
+			if (!err) {
+				//callbackFunction( response.json.results[0].address_components );
+				callbackFunction( response.json.results[0] );
+			}
+		});
+	});
+}
+
+//getMoreInfomation( function( output ){
+	//console.log(output);
+	// function tryingToOutput(){
+	// 	return output;
+	// }
+//});
+
+function outputTheResult(output){
+	console.log(output.formatted_address);
+}
+
+function writingFile( callbackFunction ){
+	var infoArray = []
+	getMoreInfomation(function( output ){	//the output is the object from reverse geocode( the address info )
+		infoArray.push(output.formatted_address);
+		console.log(houseData[6]);
+		infoArray.push(houseData[6]);
+		infoArray.push(output.geometry.location.lat);
+		infoArray.push(output.geometry.location.lng);
+
+		callbackFunction( infoArray );
+	});
+}
+
+function writeCSV( writedInfo ){	//array
+	var csvStream = csv.createWriteStream({headers: true}),
+    writableStream = fs.createWriteStream("testAddress2.csv", {defaultEncoding: 'utf8'});
+
+    csvStream.pipe(writableStream);
+    csvStream.write({googleAPI return Address: writedInfo[0], 原地址: writedInfo[1], lat: writedInfo[2], lng: writedInfo[3] });
+    csvStream.end();
+}
+
+
+
+
+/*
 function useAPIs(){
 	var latlng;
 	for (var i = 1; i < 20; i++) {
- 		console.log("WWWW");
+ 		//console.log("WWWW");
  		//houseData[i]
  		geocodeInputObject.address = houseData[i];
  		console.log(geocodeInputObject);
@@ -77,6 +178,7 @@ function useAPIs(){
  		//googleMapsClient.reverseGeocode(reverseGeocodeInputObject, reverseGeocodeCallback);
  	}
 }
+*/
 
 function geocodeCallback(err, response){
 	var str;
