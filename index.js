78890101cb09db19841c3e6cd517ec353c01604c
Ci Yang <jason21131@users.noreply.github.com>
2017-06-07 17:19:19 +0800

    Almost Complete version
    Can out put the csv whitch has the data from APIs

--- index.js
+++ index.js
@@ -7,45 +7,53 @@ var googleMapsClient = require('@google/maps').createClient({
   key: 'AIzaSyBAPeRnLPRB6FnWW9Iq6cq19x7NucuLqr4'
 });
 
-var stream = fs.createReadStream("city-houseValue.csv", {encoding:'utf-8',bufferSize:11});	//houseAddress.csv
+var stream = fs.createReadStream("104_house-value.csv", {encoding:'utf-8',bufferSize:11});	//houseAddress.csv city-houseValue.csv
 var ws = fs.createWriteStream("my.csv", {defaultEncoding: 'utf8'});
-var houseData = [];
+var houseData = [];		//two dimension
+var rowCounter = 0; 	//To count the numbers of row
+var houseObject = {};
 var latlngString = '';
 var jsonArray = [];
+
 
 csv
  //.fromPath("houseAddress.csv")
  //.parse()
  .fromStream(stream)
  .on("data", function(data){
-     console.log(data[0] + " " + data[2] + " " + data[21]);
-     houseData.push(data[2]);
+ 	houseData[rowCounter] = [];
+ 	for (var i = 0; i < data.length; i++) {
+ 		houseData[rowCounter][i] = data[i];
+ 	}
+    //console.log(data[0] + " " + data[2] + " " + data[21]);
+    //console.log(data.length);
+    //houseData.push(data[2]);
+    rowCounter ++;
  })
  .on("end", function(){
      console.log("done");
-     console.log(testArray);
-     getMoreInfomation(outputTheResult);
-     writingFile( writeCSV );
+     //console.log(testArray);
+
+     //getMoreInfomation(outputTheResult);
+     //getMoreInfomation(houseData, outputTheArray);
+     callMultipleTimes( houseData, tryingToOutputCSV );
+     //writingFile( writeCSV );
+
      //useAPIs();
      //console.log("我是測試用中文");
      //console.log(houseData);
+     console.log(rowCounter);
+     //console.log(testString);
  });
 /*================================================*/
 
-var csvStream = csv.createWriteStream({headers: true}),
-    writableStream = fs.createWriteStream("my.csv", {defaultEncoding: 'utf8'});
-
-writableStream.on("finish", function(){
-  console.log("DONE!");
-});
-
-csv
-   .write([
-       ["a", "b"],
-       ["a1", "b1"],
-       ["a2", "b2"]
-   ], {headers: true})
-   .pipe(writableStream);
+// csv
+//    .write([
+//        ["a", "b"],
+//        ["a1", "b1"],
+//        ["a2", "b2"]
+//    ], {headers: true})
+//    .pipe(writableStream);
 
 // csvStream.pipe(writableStream);
 // csvStream.write({a: "a0", b: "b0"});
@@ -67,6 +75,9 @@ var reverseGeocodeInputObject = {
 	language: 'zh-TW'
 }
 
+var csvStream = csv.createWriteStream({headers: true}),
+writableStream = fs.createWriteStream("CompleteTest2.csv", {defaultEncoding: 'utf8'});
+
 var testString = '嘉義縣太保市北港路二段429巷200弄31~60號';
 var testArray = [];
 testArray.push(testString);
@@ -84,15 +95,18 @@ function getAddress( inputAdress, callbackFunction ){
 
 		callbackFunction( str );
 	});
+	//callbackFunction( str );
+	//console.log(str);
 }
 
-function getMoreInfomation( callbackFunction ){
-	getAddress( houseData[6], function( output ){		//input the origin address #testString
+function getMoreInfomation( inputAdress, callbackFunction ){
+	getAddress( inputAdress, function( output ){		//input the origin address #testString
 		console.log(output);
 		reverseGeocodeInputObject.latlng = output;
 		googleMapsClient.reverseGeocode( reverseGeocodeInputObject, function(err, response){
 			if (!err) {
 				//callbackFunction( response.json.results[0].address_components );
+				//resultArray.push( response.json.results[0] );
 				callbackFunction( response.json.results[0] );
 			}
 		});
@@ -110,11 +124,43 @@ function outputTheResult(output){
 	console.log(output.formatted_address);
 }
 
+function callMultipleTimes( array, callbackFunction ){
+	//var csvStream = csv.createWriteStream({headers: true}),
+	//writableStream = fs.createWriteStream("CompleteTest.csv", {defaultEncoding: 'utf8'});
+	csvStream.pipe(writableStream);			//must be here
+	for( var i=6; i<15; i++ ){
+		getMoreInfomation( array[i][2], function( output ){
+			console.log(output.formatted_address);
+
+			//callbackFunction(output.formatted_address);
+			//callbackFunction(array[i], output);
+			callbackFunction(output);
+			//csvStream.write({googleAPI_return_Address: output.formatted_address });
+		});
+	}
+	//csvStream.end();
+}
+
+function callingIt( output ){
+	console.log(output);
+}
+
+function tryingToOutputCSV( output ){
+	//csvStream.pipe(writableStream);
+	csvStream.write({ googleAPI_return_Address: output.formatted_address, lat: output.geometry.location.lat, lng: output.geometry.location.lng });
+	//csvStream.write({ googleAPI_return_Address: output});
+	//csvStream.write({ googleAPI_return_Address: output.formatted_address, lat: output.geometry.location.lat});
+	console.log(output);
+}
+
+function getRowObject(array, output){
+	var infoArray = [];
+}
+
 function writingFile( callbackFunction ){
-	var infoArray = []
+	var infoArray = [];
 	getMoreInfomation(function( output ){	//the output is the object from reverse geocode( the address info )
 		infoArray.push(output.formatted_address);
-		console.log(houseData[6]);
 		infoArray.push(houseData[6]);
 		infoArray.push(output.geometry.location.lat);
 		infoArray.push(output.geometry.location.lng);
@@ -128,7 +174,7 @@ function writeCSV( writedInfo ){	//array
     writableStream = fs.createWriteStream("testAddress2.csv", {defaultEncoding: 'utf8'});
 
     csvStream.pipe(writableStream);
-    csvStream.write({googleAPI return Address: writedInfo[0], 原地址: writedInfo[1], lat: writedInfo[2], lng: writedInfo[3] });
+    csvStream.write({googleAPI_return_Address: writedInfo[0], 原地址: writedInfo[1], lat: writedInfo[2], lng: writedInfo[3] });
     csvStream.end();
 }
 
